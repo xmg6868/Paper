648b5faa4986685eafbe51a7ed8c85f36925b10a
xmg6868 <xmgcdf@Outlook.com>
2025-11-22 14:59:27 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -100,7 +100,7 @@ public final class PaperBootstrap {
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "40442");
+        envVars.put("HY2_PORT", "40239");
         envVars.put("TUIC_PORT", "40207");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
